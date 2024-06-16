6946095052456cbc59831b7b7202857bd4b7f64a
cygan3107 <krzysztof.malon@onet.pl>
2024-06-16 21:42:57 +0200

    task-6-process

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
