959dbefcab8d7eac2460afc7dc6e89566f0ef84d
cygan3107 <krzysztof.malon@onet.pl>
2024-06-16 21:01:49 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,8 @@
-const loginForm = document.querySelector(".login-form");
-loginForm.addEventListener("submit", (event) => {
+const login = document.querySelector(".login-form");
+login.addEventListener("submit", (event) => {
     event.preventDefault();
-    const email = loginForm.elements.email.value.trim();
-    const password = loginForm.elements.password.value.trim();
+    const email = login.elements.email.value.trim();
+    const password = login.elements.password.value.trim();
     if (email === "" || password === "") {
         alert("All form fields must be filled in");
         return;
@@ -12,5 +12,5 @@ loginForm.addEventListener("submit", (event) => {
         password: password,
     };
     console.log(formData);
-    loginForm.reset();
+    login.reset();
 });
